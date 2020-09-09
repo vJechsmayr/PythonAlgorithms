7f38f9aaa4bc55050749593468beb26b176a23f6
nattydeloco <nattydeloco@users.noreply.github.com>
2020-09-09 20:20:15 +0800

    Create 0065_Valid_Number.py
    Added solution, verified with LeetCode

---
+++ LeetCode/0065_Valid_Number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isNumber(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
