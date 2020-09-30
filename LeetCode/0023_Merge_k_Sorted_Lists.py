dd2e6e7346a4c3c1e10f617838494dcfc9e0f5a1
Parth Maniar <officialparthmaniar@gmail.com>
2020-10-01 00:59:11 +0530

    fix

--- LeetCode/0023_Merge_k_Sorted_Lists.py
+++ LeetCode/0023_Merge_k_Sorted_Lists.py
@@ -5,16 +5,15 @@ class Solution:
             cur = node
             while cur:
                 if cur.val not in output:
-                    # head, tail
-                    output[cur.val] = [cur,cur]
+                    output[cur.val] = [cur, cur]
                 else:
                     output[cur.val][1].next = cur
                     output[cur.val][1] = cur
                 cur = cur.next
         sorted_keys = sorted(list(output.keys()))
-        
-        for k in range(len(sorted_keys)-1):
-            output[sorted_keys[k]][1].next = output[sorted_keys[k+1]][0]
+
+        for k in range(len(sorted_keys) - 1):
+            output[sorted_keys[k]][1].next = output[sorted_keys[k + 1]][0]
         if sorted_keys:
             return output[sorted_keys[0]][0]
         else:
