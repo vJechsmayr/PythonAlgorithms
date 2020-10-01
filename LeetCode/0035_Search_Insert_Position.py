beb5fc8aaf97adba4a0ed89ee6bab25ca852f09f
shubham2704 <cool.shubham2704@gmail.com>
2020-10-02 00:51:50 +0530

    added solution for 0035 search insert position

---
+++ LeetCode/0035_Search_Insert_Position.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
