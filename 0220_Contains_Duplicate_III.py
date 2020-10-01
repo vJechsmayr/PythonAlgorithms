6f6eedecec46c5a2d5de5f01d89640f9ea1e62a0
patel kamil <38882087+kamil200@users.noreply.github.com>
2020-10-02 02:11:53 +0530

    solution -- 220. Contains Duplicate III
    fixes #86

--- 0220_Contains_Duplicate_III.py
+++ 0220_Contains_Duplicate_III.py
@@ -1,25 +1,20 @@
-class Solution{
-    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if(nums==null||nums.length<2||k<0||t<0)
-            return false;
-    
-        TreeSet<Long> set = new TreeSet<Long>();
-        for(int i=0; i<nums.length; i++){
-            long curr = (long) nums[i];
-    
-            long leftBoundary = (long) curr-t;
-            long rightBoundary = (long) curr+t+1; //right boundary is exclusive, so +1
-            SortedSet<Long> sub = set.subSet(leftBoundary, rightBoundary);
-            if(sub.size()>0)
-                return true;
-    
-            set.add(curr);   
-    
-            if(i>=k){ // or if(set.size()>=k+1)
-                set.remove((long)nums[i-k]);
-            }
-        }
-    
-        return false;
-}
-}
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        if t < 0 or not nums or k <= 0:
+            return False
+        buckets = {}
+        width = t + 1
+
+        for n, i in enumerate(nums):
+            buck = i // width
+            if buck in buckets:
+                return True
+            else:
+                buckets[buck] = i
+                if buck - 1 in buckets and i - buckets[buck-1] <= t:
+                    return True
+                if buck + 1 in buckets and buckets[buck+1] - i <= t:
+                    return True
+                if n >= k:
+                    del buckets[nums[n-k] // width]
+        return False
