1bf406ea053621339e6420340ec860e575605228
Viktoria Jechsmayr <viktoria@jechsmayr.at>
2020-10-01 18:31:01 +0200

    Create 0002_Add_Two_Numbers.py

---
+++ LeetCode/0002_Add_Two_Numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2 ,c = 0):
+        val = l1.val + l2.val + c
+        c = val // 10
+        ret = ListNode(val % 10 ) 
+        
+        if (l1.next != None or l2.next != None or c != 0):
+            if l1.next == None:
+                l1.next = ListNode(0)
+            if l2.next == None:
+                l2.next = ListNode(0)
+            ret.next = self.addTwoNumbers(l1.next,l2.next,c)
+        return ret
