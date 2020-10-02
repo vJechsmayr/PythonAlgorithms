8d552a54a8bb41797ac155215d339eadbe1c724f
Tobias Christiansen <tobi@tobyase.de>
2020-10-02 15:59:14 +0200

    Added memoization to not calculate a value twice.

--- LeetCode/0070_Climbing_Stairs.py
+++ LeetCode/0070_Climbing_Stairs.py
@@ -18,10 +18,17 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         return self.fib(n + 1)
 
+    fibStore = {}
     def fib(self, n: int) -> int:
+        if n in self.fibStore:
+            return self.fibStore[n]
+
         if n <= 2:
             return 1
-        return self.fib(n - 1) + self.fib(n - 2)
+
+        value = self.fib(n - 1) + self.fib(n - 2)
+        self.fibStore[n] = value
+        return value
 
 
-print(Solution().climbStairs(3))
+print(Solution().climbStairs(30))
