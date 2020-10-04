85a5b215c93ac7bbf1677103002cef400874e197
vickyjsr <71089444+vickyjsr@users.noreply.github.com>
2020-10-05 01:23:05 +0530

    Create swap.py

---
+++ LeetCode/swap.py
@@ -0,0 +1,14 @@
+# Python program to demonstrate 
+# swapping of two variables 
+
+x = 10
+y = 50
+
+# Swapping of two variables 
+# Using third variable 
+temp = x 
+x = y 
+y = temp 
+
+print("Value of x:", x) 
+print("Value of y:", y) 
