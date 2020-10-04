8b129b534af884a44d0ed5275c27f1a1755a10f7
Giorgi Beriashvili <giorgi.beriashvili@outlook.com>
2020-10-05 00:22:56 +0400

    Fix the solution for "0383 - Ransom Note" problem
    Resolves #264

--- LeetCode/0383_RansomNote.py
+++ LeetCode/0383_RansomNote.py
@@ -1,13 +1,6 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        length = len(magazine)
-
-        for index in range(len(ransomNote)):
-            if ransomNote[index] in magazine:
-                match = magazine.find(ransomNote[index])
-
-                magazine = magazine[:match] + magazine[match + 1 :]
-            else:
-                return False
-            if length - len(magazine) == len(ransomNote):
-                return True
+        return all(
+            ransomNote.count(letter) <= magazine.count(letter)
+            for letter in set(ransomNote)
+        )
