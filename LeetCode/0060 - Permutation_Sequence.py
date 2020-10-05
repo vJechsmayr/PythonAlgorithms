069354d31461ea41766cb40c02b71ab13641ebf3
Sourab Maity <62734958+SOURAB-BAPPA@users.noreply.github.com>
2020-10-05 08:51:26 +0530

    Update 0060 - Permutation_Sequence.py
    after LeetCode success

--- LeetCode/0060 - Permutation_Sequence.py
+++ LeetCode/0060 - Permutation_Sequence.py
@@ -1,23 +1,16 @@
+from itertools import permutations
 class Solution:
     data = []
 
     def getPermutation(self, n: int, k: int) -> str:
+        self.data = []
         string = []
         for i in range(1, n+1):
             string.append(str(i))
-        self.permutations(string, 0, len(string))
-        return self.data[k-1]
+        string = list(permutations(string))
+        temp = string[k-1]
+        string = []
+        string.append("".join(temp))
+        return string[0]
 
-    def permutations(self, string, val, length):
-        if val == length - 1:
-            self.data.append("".join(string))
-        else:
-            for i in range(val, length):
-                string[val], string[i] = string[i], string[val]
-                self.permutations(string, val + 1, length)
-                string[val], string[i] = string[i], string[val]
-
-
-user = Solution()
-output = user.getPermutation(int(input("Input: n=")), int(input("k=")))
-print("\nOutput: {}".format(output))
+   
