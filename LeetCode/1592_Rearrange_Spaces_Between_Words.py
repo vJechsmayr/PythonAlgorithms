ee72d7917c589c4299585c434378c7a5da129b5a
Wanda Juan <wandajuan@Wandas-MacBook-Pro.local>
2020-10-06 11:26:29 +0800

    improved runtime efficiency

--- LeetCode/1592_Rearrange_Spaces_Between_Words.py
+++ LeetCode/1592_Rearrange_Spaces_Between_Words.py
@@ -1,22 +1,9 @@
 class Solution:
     def reorderSpaces(self, text: str) -> str:
+        words = text.split()
+        space_cnt = text.count(' ')
         
-        if ' ' not in text:
-            return text
-        
-        words, new_word, space_cnt = [], '', 0
-        for l in text:
-            if l == ' ':
-                if new_word != '':
-                    words.append(new_word)
-                space_cnt += 1
-                new_word = ''
-            else:
-                new_word = ''.join([new_word, l])
-        if text[-1] != ' ':
-            words.append(new_word)
-        
-        if len(words) > 1:
-            return (' '* (space_cnt // (len(words)-1))).join(words)+ ' ' * (space_cnt % (len(words)-1))
+        if len(words) == 1:
+            return words[0] + ' ' * space_cnt
         else:
-            return words[0] + ' '* space_cnt
+            return (' '* (space_cnt // (len(words)-1))).join(words)+ ' ' * (space_cnt % (len(words)-1))
