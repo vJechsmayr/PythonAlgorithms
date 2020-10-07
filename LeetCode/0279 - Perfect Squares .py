216a00fd810463021c5589dedb36003549a6511b
mehdios36000 <57323885+mehdios36000@users.noreply.github.com>
2020-10-07 18:58:07 +0100

    Update 0279 - Perfect Squares .py
    Closes #435

--- LeetCode/0279 - Perfect Squares .py
+++ LeetCode/0279 - Perfect Squares .py
@@ -1,15 +1,15 @@
 class Solution:
     def numSquares(self,n):
-        dp=[0]*(n+1)
+        dpa=[0]*(n+1)
         for x in range(1,n+1):
             min_value=x
             y,square=1,1
             while square<=x:
-                min_value=min(min_value,1+dp[x-square])
+                min_value=min(min_value,1+dpa[x-square])
                 y+=1
                 square=y*y
-                dp[x]=min_value
-        return dp[n]
+                dpa[x]=min_value
+        return dpa[n]
 
 		
         
