a82a97f00ce55defb3d1ed8bb5cfaca9b064b222
Viktoria Jechsmayr <viktoria@jechsmayr.at>
2020-10-09 21:17:01 +0200

    Delete 1480_Running_Sum_of_1d_Array.py
    Problem already solved

--- LeetCode/1480_Running_Sum_of_1d_Array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        answers=[]
-        answers.append(nums[0])
-        for i in range(1,len(nums)):
-            answers.append(answers[-1]+nums[i])
-        return answers
