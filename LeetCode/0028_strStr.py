07624642411a3f4f87c972e1b3c7dd4571c48385
kevin454475 <71903670+kevin454475@users.noreply.github.com>
2020-10-10 13:39:23 -0400

    Delete 0028_strStr.py

--- LeetCode/0028_strStr.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        try:
-            res = haystack.index(needle)
-            return res
-        except:
-            return -1 if needle else 0
