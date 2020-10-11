ee17de30953a4cc03ad7be856e19587a6a12235a
Tushar Sethi <sethitushar2000@gmail.com>
2020-10-11 12:49:18 +0530

    Added 1299_Replace_Elements_with_Greatest_Elements_on_Right_Side.py

--- LeetCode/1299_Replace_Elements_with_Greatest_Elements_on_Right_Side.py
+++ LeetCode/1299_Replace_Elements_with_Greatest_Elements_on_Right_Side.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def replaceElements(self, arr):
-        arr1 = []
         for i in range(len(arr)):
             if i==len(arr)-1:
-                arr1.append(-1)
+                arr[i] = -1
             else:
-                arr1.append(max(arr[i+1:]))
-        return arr1
+                arr[i] = max(arr[i+1:])
+        return
