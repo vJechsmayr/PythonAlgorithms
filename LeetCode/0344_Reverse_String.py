91b7672cd3fea77a6faf1835d43e68627d89f06f
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2020-10-13 13:48:31 +0530

    0344_Reverse_String

---
+++ LeetCode/0344_Reverse_String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s):
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left + 1, right - 1)
+
+        helper(0, len(s) - 1)
