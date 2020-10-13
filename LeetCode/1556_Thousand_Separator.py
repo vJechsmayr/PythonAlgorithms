f2a83e4363c313a314ab75d58a213e6fba7b67fd
Dev.ruji <43924465+devruji@users.noreply.github.com>
2020-10-13 18:08:44 +0700

    Changed comma to dot

--- LeetCode/1556_Thousand_Separator.py
+++ LeetCode/1556_Thousand_Separator.py
@@ -1,11 +1,11 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
         num = n
-        ans = f'{num:,}'
+        ans = f'{num:,}'.replace(',', '.')
         return ans
 
 
 if __name__ == '__main__':
     x = Solution()
-    answer = x.thousandSeparator(2**31)
+    answer = x.thousandSeparator(1234)
     print(answer)
