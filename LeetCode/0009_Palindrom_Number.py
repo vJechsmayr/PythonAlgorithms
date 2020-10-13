4aecd79340a1a24d549716d2fc78656d7f115865
Fahmiady37 <asfahmi5@gmail.com>
2020-10-13 14:29:15 +0700

    justifies the palindrome

--- LeetCode/0009_Palindrom_Number.py
+++ LeetCode/0009_Palindrom_Number.py
@@ -1,12 +1,55 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x > 0 and x < 10:
-            return True
+def createdeque():
+    d = []
+    return (d)
 
-        copyNumber = x
-        newNumber = 0
-        while copyNumber > 0:
-            newNumber = int(newNumber * 10 + (copyNumber % 10))
-            copyNumber = int(copyNumber / 10)
 
-        return newNumber == x
+def addfront(d, data):
+    d.insert(0, data)
+    return (d)
+
+
+def addrear(d, data):
+    d.append(data)
+    return (d)
+
+
+def removerear(d):
+    data = d.pop()
+    return (data)
+
+
+def removefront(d):
+    data = d.pop(0)
+    return (data)
+
+
+def isempty(d):
+    return (d == [])
+
+
+def size(d):
+    return (len(d))
+
+
+def palindrom(kata):
+    data = createdeque()
+    for ch in kata:
+        addfront(data, ch)
+    cek = True
+    while size(data) > 1:
+        if removefront(data) == removerear(data):
+            cek = cek and True
+        else:
+            cek = cek and False
+    return cek
+
+
+x = "yes"
+while x == "yes":
+    h = input("property of the word to be checked:")
+    k = palindrom(h)
+    if k == True:
+        print("the word is a palindrome:")
+    else:
+        print("word not palindrome")
+    x = input("do you want more yes/no:")
