6b25110e1797afe9e082eed7a631c6e95545f1aa
Achyut-sudo <71660666+Achyut-sudo@users.noreply.github.com>
2020-10-29 22:08:19 +0530

    Added code to .py file

--- LeetCode/0030_Substring_with_Concatenation_of_All_Words.py
+++ LeetCode/0030_Substring_with_Concatenation_of_All_Words.py
@@ -1,1 +1,28 @@
 
+class Solution:
+
+    def findSubstring(self,s, words):
+        self.s = s
+        self.words = words
+        self.WordsLen = len(words[0])
+        self.index = 0
+        self.Leng = len(s)     
+        self.output = []
+        self.maxIndex = len(self.s) + 1 - self.WordsLen
+        while self.index <= self.maxIndex :
+            TempInd = self.index
+            WordsCopy = self.words.copy()
+            while TempInd <= self.maxIndex :
+                tempStr = self.s[TempInd :TempInd + self.WordsLen]
+                if tempStr in WordsCopy :
+                    WordsCopy.remove(tempStr)
+                    TempInd += self.WordsLen
+                else:
+                    break
+            if WordsCopy == [] :
+                print(self.index)
+                self.output.append(self.index)
+            self.index += 1
+        return self.output
+
+ 
